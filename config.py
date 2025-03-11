996d94dc231020aa4580e9ba56e74330c210a8f7
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2025-03-11 15:47:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ import os
 
 
 class Config:
-#    API_ID=os.getenv('api_id')
-#    API_HASH=os.getenv('api_hash')
-    API_ID = 23168749  
-    API_HASH = "da8d6a6b81e2e7af6553c1fdccc0abde"
+    API_ID=os.getenv('api_id')
+    API_HASH=os.getenv('api_hash')
